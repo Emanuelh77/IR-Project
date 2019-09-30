bb6ab178432438b2ae8c545517bfda8f78196293
E-man <emanuelh77@hotmail.com>
2019-09-30 00:46:46 -0600

    learning to integrate frames together

--- searchpage_ui.py
+++ searchpage_ui.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
-class GoFind(tk.Tk):
-    def __init__(self):
+LARGE_FONT = ("Verdana", 12)
 
+#Inherits from tk.Tk
+class GoFind(tk.Tk):
+
+    def __init__(self):
+        #Initializing tk.Tk as well
         tk.Tk.__init__(self)
         container = tk.Frame(self)
-
         container.pack(side='top', fill='both', expand=True)
 
         container.grid_rowconfigure(0, weight=1)
@@ -13,10 +16,41 @@ class GoFind(tk.Tk):
 
         self.frames = {}
 
-        frame = StartPage(container, self)
+        for F in(StartPage, SearchPage):
+            frame = F(container, self)
+            self.frames[F] = frame
 
-        self.frames[StartPage] = frame
-
-        frame.grid(row=0, column=0, stick="nsew")
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+def placeholder(msg):
+    print(msg)
+
+class StartPage(tk.Frame):
+
+    def __init__(self, master, controller):
+        tk.Frame.__init__(self, master)
+        label = tk.Label(self, text="GoFind", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        search_button = tk.Button(self, text="Search", command=lambda: controller.show_frame(SearchPage))
+        search_button.pack()
+
+class SearchPage(tk.Frame):
+
+    def __init__(self, master, controller):
+        tk.Frame.__init__(self, master)
+        label = tk.Label(self, text="Results", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        search_button = tk.Button(self, text="Search", command=lambda: controller.show_frame(StartPage))
+        search_button.pack()
+
+app = GoFind()
+app.mainloop()
