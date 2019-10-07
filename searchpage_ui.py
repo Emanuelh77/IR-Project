e2e18a87a1e91b7166d891e7bced63a851e1d358
E-man <emanuelh77@hotmail.com>
2019-10-06 20:36:44 -0600

    start page and search page roughly layed out

--- searchpage_ui.py
+++ searchpage_ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk, Entry
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -6,8 +7,10 @@ LARGE_FONT = ("Verdana", 12)
 class GoFind(tk.Tk):
 
     def __init__(self):
-        #Initializing tk.Tk as well
         tk.Tk.__init__(self)
+
+        tk.Tk.wm_title(self, "GoFind")
+
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
 
@@ -36,10 +39,14 @@ class StartPage(tk.Frame):
 
     def __init__(self, master, controller):
         tk.Frame.__init__(self, master)
+
         label = tk.Label(self, text="GoFind", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        search_button = tk.Button(self, text="Search", command=lambda: controller.show_frame(SearchPage))
+        entry = Entry(self)
+        entry.pack()
+
+        search_button = ttk.Button(self, text="Search", command=lambda: controller.show_frame(SearchPage))
         search_button.pack()
 
 class SearchPage(tk.Frame):
@@ -47,10 +54,13 @@ class SearchPage(tk.Frame):
     def __init__(self, master, controller):
         tk.Frame.__init__(self, master)
         label = tk.Label(self, text="Results", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        label.grid(row=0, column=0, sticky="w")
 
-        search_button = tk.Button(self, text="Search", command=lambda: controller.show_frame(StartPage))
-        search_button.pack()
+        entry = Entry(self)
+        entry.grid(row=1, column=0, ipadx=50, padx=.75) #ipadx=how long the entry is, padx=padding on the x axis
+
+        search_button = ttk.Button(self, text="Search", command=lambda: controller.show_frame(StartPage))
+        search_button.grid(row=1, column=1, padx=10)
 
 app = GoFind()
 app.mainloop()
